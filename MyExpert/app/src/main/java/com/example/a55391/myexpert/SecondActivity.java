b44410c1aebd8185e812a25607a0b10bfdf15437
55391 <553913214@qq.com>
2017-11-06 23:45:37 +0800

    import   my new project 4

---
+++ MyExpert/app/src/main/java/com/example/a55391/myexpert/SecondActivity.java
@@ -0,0 +1,18 @@
+package com.example.a55391.myexpert;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SecondActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        int i=0;
+        int J=123;
+        System.out.println("nihao");
+
+    }
+}
